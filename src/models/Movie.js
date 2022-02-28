bbeb74fe33e1faa39968e3f61aa5323dbfbd62b3
Rocky Cifu <rocky.cifu@launchacademy.co>
2022-02-28 10:43:51 -0800

    Starter code for Monday clinic

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,7 +1,6 @@
 import fs from "fs"
 import _ from "lodash"
 
-//Establish path to json file outside class
 const moviesPath = "movies.json"
 
 class Movie {
@@ -12,31 +11,26 @@ class Movie {
   }
 
   static findAll() {
-    //read in the file
     const stringifiedData = fs.readFileSync(moviesPath)
-    //parse to json
     const movieData = JSON.parse(stringifiedData).movies
-    //create Movie objects from data
     const movies = movieData.map((movie) => new Movie(movie))
-    //return array of Movie objects
     return movies
   }
 
+  static findById(id) {
+    const movies = Movie.findAll()
+    return movies.find((movie) => movie.id === id)
+  }
+
   static getNextMovieId() {
-    //use lodash to find the movie with highest id
     const maxMovie = _.maxBy(this.findAll(), (movie) => movie.id)
-    // then we increment the id by 1
     return maxMovie.id + 1
   }
 
   save() {
-    //set id
     this.id = Movie.getNextMovieId()
-    //get current movies
     const currentMovies = Movie.findAll()
-    //push this to movies array
     currentMovies.push(this)
-    //write to json file
     fs.writeFileSync(moviesPath, JSON.stringify({ movies: currentMovies }))
   }
 }
--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -1,16 +1,9 @@
 import express from "express"
 
-import moviesRouter from "./moviesRouter.js"
-
 const rootRouter = new express.Router()
 
-//at root of site ('/')
 rootRouter.get("/", (req, res) => {
-  //redirect to url '/movies'
-  res.redirect("/movies")
+  res.send("Movie Database")
 })
-
-//if url path is /movies, the moviesRouter will take it from there
-rootRouter.use("/movies", moviesRouter)
 
 export default rootRouter
